ff494ba8e1301809ac7df8f92e1f05f68ef28e73
jsam2412 <j.sam2412@gmail.com>
2017-08-28 12:51:03 +0530

    Add .net core application
    Add .net core application

---
+++ mytestapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mytestapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
